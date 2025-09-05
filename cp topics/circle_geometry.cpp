48d24c093b24b781ae73e3ebc62eddd6cc0892b2
NurShuv0 <shuvo.nuru@gmail.com>
2025-09-05 17:35:51 +0600

    cf solves

---
+++ cp topics/circle_geometry.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define let_strt ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define nl "\n"
+#define int long long
+#define sp ' '
+#define pi 3.14159265358979323846
+#define square(n) (n) * (n)
+#define cube(n) (n) * (n) * (n)
+#define setPrec(n) fixed << setprecision(n)
+#define circulate(a, b) (((a % b) + b) % b)
+#define pb push_back
+#define f first
+#define s second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define cn continue
+#define mod 1000000007
+#define best INT_MAX
+#define wrst INT_MIN
+// int gcd (int a,int b) {return __gcd(a,b);}
+// int lcm (int a,int b ) {return a * (b/gcd(a,b));}
+using namespace std;
+signed main() 
+{
+    let_strt
+    int r, x1,y1, x2, y2;
+    cin >> r >> x1 >> y1 >> x2 >> y2;
+    double distance = sqrt(pow(x1 - x2,2) + pow(y1 - y2,2));
+   // cout << distance << nl;
+    int ans = ceil(distance/(double)(2 * r));
+    cout << ans << nl;
+}
