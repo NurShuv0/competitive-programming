c108c76b88dd3453d0f7a8b23c10a4de770f5e1d
NurShuv0 <shuvo.nuru@gmail.com>
2025-10-08 04:11:28 +0600

    cf solves

---
+++ codeforces  solves/competitive_programmer.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define let_strt ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define nl "\n"
+#define int long long
+#define sp ' '
+#define pi 3.14159265358979323846
+#define square(n) (n) * (n)
+#define cube(n) (n) * (n) * (n)
+#define setPrec(n) fixed << setprecision(n)
+#define circulate(a, b) (((a % b) + b) % b)
+#define pb push_back
+#define f first
+#define s second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define cn continue
+#define mod 1000000007
+#define best LLONG_MAX
+#define wrst LLONG_MIN
+// int gcd (int a,int b) {return __gcd(a,b);}
+// int lcm (int a,int b ) {return a * (b/gcd(a,b));}
+using namespace std;
+signed main()
+{
+    let_strt;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string n;
+        cin >> n;
+        int sum = 0, zero = 0, even = 0;
+        for (char c : n)
+        {
+            int digit = c - '0';
+            sum += digit;
+            if (digit == 0)
+                zero++;
+            if (digit % 2 == 0)
+                even++;
+        }
+        cout << (zero >= 1 && even >= 2 && sum % 3 == 0 ? "red" : "cyan") << nl;
+    }
+    return 0;
+}
--- codeforces  solves/pp.cpp
+++ codeforces  solves/pp.cpp
@@ -1,69 +1,48 @@
 #include <bits/stdc++.h>
 #define let_strt ios_base::sync_with_stdio(false); cin.tie(nullptr);
 #define nl "\n"
-#define int long long
-#define sp ' '
-#define pi 3.14159265358979323846
-#define square(n) (n) * (n)
-#define cube(n) (n) * (n) * (n)
-#define setPrec(n) fixed << setprecision(n)
-#define circulate(a, b) (((a % b) + b) % b)
-#define pb push_back
-#define f first
-#define s second
-#define all(v) v.begin(), v.end()
-#define allr(v) v.rbegin(), v.rend()
-#define cn continue
-#define mod 1000000007
-#define best LLONG_MAX
-#define wrst LLONG_MIN
 using namespace std;
 
-signed main()
-{
+int main() {
     let_strt;
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n;
         cin >> n;
-        string str;
-        cin >> str;
+        string s;
+        cin >> s;
 
-        int cnt_a = count(all(str), 'a');
-        int cnt_b = n - cnt_a;
+        int total_a = count(s.begin(), s.end(), 'a');
+        int total_b = n - total_a;
 
-        if (cnt_a == cnt_b)
-        {
+        if (total_a == total_b) {
             cout << 0 << nl;
-            cn;
+            continue;
         }
-        if (cnt_a == 0 || cnt_b == 0)
-        {
+        if (total_a == 0 || total_b == 0) {
             cout << -1 << nl;
-            cn;
+            continue;
         }
 
-        int diff = cnt_a - cnt_b; 
-        int ans = best;
-        for (int i = 0; i < n; i++)
-        {
-            int a_removed = 0, b_removed = 0;
-            for (int j = i; j < n; j++)
-            {
-                if (str[j] == 'a') a_removed++;
-                else b_removed++;
+        unordered_map<int, int> first_occ;
+        int diff = 0;
+        first_occ[0] = -1;
+        int min_len = n + 1;
 
-                if (a_removed - b_removed == diff)
-                    ans = min(ans, j - i + 1);
+        for (int i = 0; i < n; ++i) {
+            diff += (s[i] == 'a' ? 1 : -1);
+            if (first_occ.count(diff)) {
+                min_len = min(min_len, i - first_occ[diff]);
+            } else {
+                first_occ[diff] = i;
             }
         }
 
-        if (ans == best || ans == n)
+        if (min_len > n)
             cout << -1 << nl;
         else
-            cout << ans << nl;
+            cout << min_len << nl;
     }
     return 0;
 }
