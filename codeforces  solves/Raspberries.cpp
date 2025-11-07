abca37e38d74f0d49a5c0b0aea74a9ba50fad5bb
NurShuv0 <shuvo.nuru@gmail.com>
2025-11-08 03:45:40 +0600

    tle elimators

---
+++ codeforces  solves/Raspberries.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define let_strt ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define nl "\n"
+#define int long long
+#define sp ' '
+#define pi 3.14159265358979323846
+#define square(n) (n) * (n)
+#define cube(n) (n) * (n) * (n)
+#define setPrec(n) fixed << setprecision(n)
+#define circulate(a, b) (((a % b) + b) % b)
+#define pb push_back
+#define f first
+#define s second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define cn continue
+#define mod 1000000007
+#define best LLONG_MAX
+#define wrst LLONG_MIN
+// int gcd (int a,int b) {return __gcd(a,b);}
+// int lcm (int a,int b ) {return a * (b/gcd(a,b));}
+using namespace std;
+signed main() 
+{
+    let_strt;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int>v(n);
+        int even = 0;
+        bool flag = false;
+        for(int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            if(v[i] % 2 == 0)
+            {
+                even++;
+            }
+            if(v[i] % k == 0)
+            {
+                flag = true;
+            }
+        }
+        if(flag)
+        {
+            cout << 0 << nl;
+            cn;
+        }
+        if(even > 0 && k == 2)
+        {
+            cout << 0 << nl;
+            cn;
+        }
+        if(even >= 2 && k == 4)
+        {
+            cout << 0 << nl;
+            cn;
+        }
+        int ans = best;
+        for(int i = 0; i < n; i++)
+        {
+            ans = min(ans, k - (v[i] % k));
+        }
+        if(k == 4)
+        {
+            ans = min(ans, 2 - even);
+        }
+        cout << ans << nl;
+
+    }
+    return 0;
+}
