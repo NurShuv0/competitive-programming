d2a5d383b0387b417a864600d18f053a2b850af4
NurShuv0 <shuvo.nuru@gmail.com>
2025-11-28 22:22:31 +0600

    cf contest

---
+++ codeforces  solves/addSegment.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define let_strt ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define nl "\n"
+#define int long long
+#define sp ' '
+#define pi 3.14159265358979323846
+#define square(n) (n) * (n)
+#define cube(n) (n) * (n) * (n)
+#define setPrec(n) fixed << setprecision(n)
+#define circulate(a, b) (((a % b) + b) % b)
+#define pb push_back
+#define f first
+#define s second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define cn continue
+#define mod 1000000007
+#define best LLONG_MAX
+#define wrst LLONG_MIN
+// int gcd (int a,int b) {return __gcd(a,b);}
+// int lcm (int a,int b ) {return a * (b/gcd(a,b));}
+using namespace std;
+signed main() 
+{
+    let_strt;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>v(n);
+        int sum = 0;
+        set<int>st;
+        int zero = 0;
+        for(int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            st.insert(v[i]);
+            sum += v[i];
+            if(v[i] == 0)
+            {
+                zero++;
+            }
+        }
+        // if(st.size() == 1)
+        // {
+        //     cout << 1 << nl;
+        //     cn;
+        // }
+        int ans = min(n - (n - (n-zero)), sum - n + 1);
+        cout << ans << nl;
+    }
+    return 0;
+}
