63b44c915fa9172f721178222b5e75c7cf2eaef5
NurShuv0 <shuvo.nuru@gmail.com>
2025-11-30 03:07:55 +0600

    TNT contest

---
+++ codeforces  solves/3sum.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define let_strt ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define nl "\n"
+#define int long long
+#define sp ' '
+#define pi 3.14159265358979323846
+#define square(n) (n) * (n)
+#define cube(n) (n) * (n) * (n)
+#define setPrec(n) fixed << setprecision(n)
+#define circulate(a, b) (((a % b) + b) % b)
+#define pb push_back
+#define f first
+#define s second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define cn continue
+#define mod 1000000007
+#define best LLONG_MAX
+#define wrst LLONG_MIN
+// int gcd (int a,int b) {return __gcd(a,b);}
+// int lcm (int a,int b ) {return a * (b/gcd(a,b));}
+using namespace std;
+signed main() 
+{
+    let_strt;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>v(n);
+        map<int,int>mp;
+        for(int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            mp[v[i] % 10]++;
+        }
+        // for(auto& c: mp)
+        // {
+        //     cout << c.f << sp << c.s << nl;
+        // }
+        vector<int>V;
+        for(int i = 0; i < 10; i++)
+        {
+            int range = min(mp[i], 3LL);
+            while(range--)
+            {
+                V.pb(i);
+            }
+        }
+        bool flag = false;
+        n = V.size();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                for(int k = j + 1; k < n; k++)
+                {
+                    if((V[i] + V[j] + V[k]) % 10 == 3)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag)
+                {
+                    break;
+                }
+            }
+            if(flag){
+                break;
+            }
+        }
+        cout << (flag?"YES":"NO") << nl;
+
+    }
+    return 0;
+}
