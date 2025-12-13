bc493be4be898e8aa2b625cc1a88118c789b0738
NurShuv0 <shuvo.nuru@gmail.com>
2025-12-14 01:37:31 +0600

    cf solves

---
+++ codeforces  solves/optimalShift.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define let_strt ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define nl "\n"
+#define int long long
+#define sp ' '
+#define pi 3.14159265358979323846
+#define square(n) (n) * (n)
+#define cube(n) (n) * (n) * (n)
+#define setPrec(n) fixed << setprecision(n)
+#define circulate(a, b) (((a % b) + b) % b)
+#define pb push_back
+#define f first
+#define s second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define cn continue
+#define mod 1000000007
+#define best LLONG_MAX
+#define wrst LLONG_MIN
+// int gcd (int a,int b) {return __gcd(a,b);}
+// int lcm (int a,int b ) {return a * (b/gcd(a,b));}
+using namespace std;
+signed main() 
+{
+    let_strt;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        int prevz = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(str[i] == '1')
+            {
+                break;
+            }
+            else {
+                prevz++;
+            }
+        }
+        int lastz = 0;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(str[i] == '1')
+            {
+                break;
+            }
+            else {
+                lastz++;
+            }
+        }
+        int cir_zero = lastz + prevz;
+        int cnt = 0;
+        int zero = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(str[i] == '1')
+            {
+                zero = max(zero, cnt);
+                cnt = 0;
+            }
+            else {
+                cnt++;
+            }
+        }
+        cout << max(zero, cir_zero) << nl;
+    }
+    return 0;
+}
