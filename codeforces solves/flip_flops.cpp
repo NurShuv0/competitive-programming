b88b1b4057315be7ed162c2f6ceb69265e262307
Nur Shuv0 <shuvo.nuru@gmail.com>
2026-03-29 02:44:54 +0600

    cf solves

---
+++ codeforces solves/flip_flops.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define let_strt ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define nl "\n"
+#define int long long
+#define sp ' '
+#define pi 3.14159265358979323846
+#define square(n) (n) * (n)
+#define cube(n) (n) * (n) * (n)
+#define setPrec(n) fixed << setprecision(n)
+#define circulate(a, b) (((a % b) + b) % b)
+#define pb push_back
+#define f first
+#define s second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define cn continue
+#define mod 1000000007
+#define best LLONG_MAX
+#define wrst LLONG_MIN
+// int gcd (int a,int b) {return __gcd(a,b);}
+// int lcm (int a,int b ) {return a * (b/gcd(a,b));}
+using namespace std;
+signed main() 
+{
+    let_strt;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, c, k;
+        cin >> n >> c >> k;
+        vector<int>v(n);
+        for(int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        int mx = 0;
+        int sum = 0;
+        vector<int>temp;
+        for(int i = 0; i < n; i++)
+        {
+            if(v[i] <= c)
+            {
+                mx += (c - v[i]);
+                sum += v[i];
+            }
+            else{
+                temp.pb(v[i]);
+            }
+        }
+        sort(all(temp));
+        int ans = c;
+        ans += (min(mx, k));
+        ans += sum;
+        for(int i = 0; i < temp.size(); i++)
+        {
+            if(temp[i] <= ans)
+            {
+                ans += temp[i];
+            }
+            // else {
+            //     break;
+            // }
+        }
+        if(k - mx > 0 && ans >= k - mx)
+        {
+            ans += (k-mx);
+        }
+        cout << ans << nl;
+    }
+    return 0;
+}
